814f2265070155e2662e0d74650688e555a59794
efimerotiempo <polaroidomana@gmail.com>
2025-07-08 14:36:35 +0200

    fix missing jsonify import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from datetime import date, timedelta, datetime
 import uuid
 import os
